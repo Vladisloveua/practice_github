1188f971ab60bc1f551dfeffadc7ed839baca865
Kateryna Ostrynska <kateriinag@gmail.com>
2022-11-22 23:11:33 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
-export const App = () => {
+import React, { useState } from 'react';
+
+function App() {
+  const list = [
+    'Banana',
+    'Apple',
+    'Orange',
+    'Mango',
+    'Pineapple',
+    'Watermelon',
+  ];
+
+  const [filterList, setFilterList] = useState(list);
+  console.log([setFilterList]);
+
+  const handleSearch = event => {
+    if (event.target.value === '') {
+      setFilterList(list);
+      return;
+    }
+
+    const filteredValues = list.filter(
+      item =>
+        item.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1
+    );
+    setFilterList(filteredValues);
+  };
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <div>
+        Search: <input name="query" type="text" onChange={handleSearch} />
+      </div>
+      {filterList &&
+        filterList.map((item, index) => <div key={index}>{item}</div>)}
     </div>
   );
-};
+}
+
+export default App;
